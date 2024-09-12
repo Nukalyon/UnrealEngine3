29416261ce75e5c2e0a436e22ece8bdbd7b68191
Nukalyon <56563361+Nukalyon@users.noreply.github.com>
2024-09-12 11:02:01 -0400

    Laser

---
+++ Cours01_bis/Intermediate/Build/Win64/UnrealEditor/Inc/Cours01_bis/UHT/LasorTrap.generated.h
@@ -0,0 +1,54 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "LasorTrap.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef COURS01_BIS_LasorTrap_generated_h
+#error "LasorTrap.generated.h already included, missing '#pragma once' in LasorTrap.h"
+#endif
+#define COURS01_BIS_LasorTrap_generated_h
+
+#define FID_Cours01_bis_Source_Cours01_bis_Private_LasorTrap_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesALasorTrap(); \
+	friend struct Z_Construct_UClass_ALasorTrap_Statics; \
+public: \
+	DECLARE_CLASS(ALasorTrap, AActor, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/Cours01_bis"), NO_API) \
+	DECLARE_SERIALIZER(ALasorTrap)
+
+
+#define FID_Cours01_bis_Source_Cours01_bis_Private_LasorTrap_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	ALasorTrap(ALasorTrap&&); \
+	ALasorTrap(const ALasorTrap&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ALasorTrap); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ALasorTrap); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(ALasorTrap) \
+	NO_API virtual ~ALasorTrap();
+
+
+#define FID_Cours01_bis_Source_Cours01_bis_Private_LasorTrap_h_9_PROLOG
+#define FID_Cours01_bis_Source_Cours01_bis_Private_LasorTrap_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Cours01_bis_Source_Cours01_bis_Private_LasorTrap_h_12_INCLASS_NO_PURE_DECLS \
+	FID_Cours01_bis_Source_Cours01_bis_Private_LasorTrap_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> COURS01_BIS_API UClass* StaticClass<class ALasorTrap>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_Cours01_bis_Source_Cours01_bis_Private_LasorTrap_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
